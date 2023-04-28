936aec25ed34ae502733ad649df289c1dbf2a737
baidyprod <dima220920041@gmail.com>
2023-04-29 02:03:24 +0300

    Commit 9 - Split settings

--- core/settings/base.py
+++ core/settings/base.py
@@ -82,11 +82,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'

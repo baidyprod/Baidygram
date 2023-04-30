0ff31b63f0fb25028defe702e2a11749bbe48e0e
baidyprod <dima220920041@gmail.com>
2023-04-30 16:43:27 +0300

    Commit 13 - Htmls structure update

--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from blog.views import BlogPostDetailView, BlogPostListView, BlogPostCreateView, BlogPostUpdateView, \
+from blog.views import BlogPostCreateView, BlogPostDetailView, BlogPostListView, BlogPostUpdateView, \
     CommentCreateView, RegisterFormView, UpdateProfile, UserProfile, contact_us
 
 from django.conf import settings
